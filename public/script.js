48f7132608eb4ccaf117fb83ced9574893fed89e
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-03-25 19:49:08 -0400

    add target box selector

--- public/script.js
+++ public/script.js
@@ -2,8 +2,11 @@ async function populateRestaurants() {
   const diningRequest = await fetch('/api/dining');
   const diningData = await diningRequest.json();
 
+  const targetBox = document.querySelector('.targetBox');
+
+
   diningData.data.forEach((restaurant) => {
-    const appendItem = document.createElement('div');
+    const appendItem = document.createElement("div");
     appendItem.classList.add('tile', 'has-text-centered', 'is-parent', 'is-3');
     appendItem.innerHTML = `
         <article class="tile is-child box has-background-link-dark">
@@ -14,28 +17,11 @@ async function populateRestaurants() {
         `;
     targetBox.append(appendItem);
   });
-
-
-//   const hallID = document.querySelector('.hallID');
-//   const hallName = document.querySelector('.hallName');
-//   const hallAddress = document.querySelector('.hallAddress');
-//   const targetTable = document.querySelector('.targetTable');
-
-//   const request = await fetch('/api/dining');
-//   const data = await request.json();
-
-//   const html = data.forEach((item) => `
-//     <td><span class="hallID">${item.hall_id}</span></td>
-//     <td><span class="hallName">${item.hall_name}</span></td>
-//     <td><span class="hallAddress">${item.hall_address}</span></td>
-//     `).join('');
-//   targetTable.innerHTML = html;
-}
+};
 
 async function windowActions() {
   await populateRestaurants();
+  console.log("loaded");
 }
 
 window.onload = windowActions;
-
-// code from thurs lecture
