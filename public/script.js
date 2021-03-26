cb0ba245c347e5e47bbb4c0a7b9b5c5c9a60d787
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-03-25 20:39:43 -0400

    split up code to append to each table cell

--- public/script.js
+++ public/script.js
@@ -2,26 +2,35 @@ async function populateRestaurants() {
   const diningRequest = await fetch('/api/dining');
   const diningData = await diningRequest.json();
 
-  const targetBox = document.querySelector('.targetBox');
-
+  const hallID = document.querySelector('.hallID');
+  const hallName = document.querySelector('.hallName');
+  const hallAddress = document.querySelector('.hallAddress');
 
   diningData.data.forEach((restaurant) => {
-    const appendItem = document.createElement("div");
-    appendItem.classList.add('tile', 'has-text-centered', 'is-parent', 'is-3');
-    appendItem.innerHTML = `
-        <article class="tile is-child box has-background-link-dark">
-        <span class="subtitle has-text-light has-text-weight-bold">${restaurant.hall_name}</span><br/>
-        <span class="has-text-light">${restaurant.hall_address.split(',')[0]}</span><br/>
-        <span class="has-text-light">${restaurant.hall_address.split(',')[1]}</span>
-        </article>
-        `;
-    targetBox.append(appendItem);
+    const appendID = document.createElement('td');
+    const appendName = document.createElement('td');
+    const appendAddress = document.createElement('td');
+
+    appendID.innerHTML = `
+    <span>${restaurant.hall_id}</span>
+    `;
+    appendName.innerHTML = `
+    <span>${restaurant.hall_name}</span>
+    `;
+    appendAddress.innerHTML = `
+    <span>${restaurant.hall_address.split(',')[0]}</span>
+    <span>${restaurant.hall_address.split(',')[1]}</span>
+    `;
+    
+    hallID.append(appendID);
+    hallName.append(appendName);
+    hallAddress.append(appendAddress);
   });
-};
+}
 
 async function windowActions() {
   await populateRestaurants();
-  console.log("loaded");
+  console.log('loaded');
 }
 
 window.onload = windowActions;
