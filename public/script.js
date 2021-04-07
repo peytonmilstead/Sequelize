1ac2b3f4f0e62a62af98f039507e046912fcb882
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-06 20:16:04 -0400

    add chart js code from tutorial

--- public/script.js
+++ public/script.js
@@ -1,3 +1,28 @@
+window.onload = function () {
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    title: {
+      text: 'Meal Macro Information'
+    },
+    axisX: {
+      valueFormatString: 'DDD'
+    },
+    axisY: {
+      prefix: '$'
+    }
+  });
+
+  chart.render();
+  function toggleDataSeries(e) {
+    if (typeof (e.dataSeries.visible) === 'undefined' || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    chart.render();
+  }
+};
+
 async function populateRestaurants() {
   const diningRequest = await fetch('/api/dining');
   const diningData = await diningRequest.json();
