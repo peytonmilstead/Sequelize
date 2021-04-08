daf9b1520f32066e4549614c6e32df3f96330fc5
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-08 17:02:45 -0400

    add index to data values for all macros

--- public/script.js
+++ public/script.js
@@ -55,37 +55,103 @@ async function macroChart() {
       // xValueFormatString: '##########',
       // yValueFormatString: '####',
       // dataPoints: mealCal
-      dataPoints: [{ x: selectedMeals[0].calories, y: selectedMeals[0].meal_name }]
+      dataPoints: [
+        { y: selectedMeals[0].calories },
+        { y: selectedMeals[1].calories },
+        { y: selectedMeals[2].calories },
+        { y: selectedMeals[3].calories },
+        { y: selectedMeals[4].calories },
+        { y: selectedMeals[5].calories },
+        { y: selectedMeals[6].calories },
+        { y: selectedMeals[7].calories },
+        { y: selectedMeals[8].calories },
+        { y: selectedMeals[9].calories }
+      ]
     },
     {
       type: 'stackedBar',
       name: 'Carbs',
       showInLegend: 'true',
-      dataPoints: [{ x: selectedMeals[0].carbs, y: selectedMeals[0].meal_name }]
+      dataPoints: [
+        { y: selectedMeals[0].carbs },
+        { y: selectedMeals[1].carbs },
+        { y: selectedMeals[2].carbs },
+        { y: selectedMeals[3].carbs },
+        { y: selectedMeals[4].carbs },
+        { y: selectedMeals[5].carbs },
+        { y: selectedMeals[6].carbs },
+        { y: selectedMeals[7].carbs },
+        { y: selectedMeals[8].carbs },
+        { y: selectedMeals[9].carbs }
+      ]
     },
     {
       type: 'stackedBar',
       name: 'Protein',
       showInLegend: 'true',
-      dataPoints: [{ x: selectedMeals[0].protein, y: selectedMeals[0].meal_name }]
+      dataPoints: [
+        { y: selectedMeals[0].protein },
+        { y: selectedMeals[1].protein },
+        { y: selectedMeals[2].protein },
+        { y: selectedMeals[3].protein },
+        { y: selectedMeals[4].protein },
+        { y: selectedMeals[5].protein },
+        { y: selectedMeals[6].protein },
+        { y: selectedMeals[7].protein },
+        { y: selectedMeals[8].protein },
+        { y: selectedMeals[9].protein }
+      ]
     },
     {
       type: 'stackedBar',
       name: 'Fat',
       showInLegend: 'true',
-      dataPoints: [{ x: selectedMeals[0].fat, y: selectedMeals[0].meal_name }]
+      dataPoints: [
+        { y: selectedMeals[0].fat },
+        { y: selectedMeals[1].fat },
+        { y: selectedMeals[2].fat },
+        { y: selectedMeals[3].fat },
+        { y: selectedMeals[4].fat },
+        { y: selectedMeals[5].fat },
+        { y: selectedMeals[6].fat },
+        { y: selectedMeals[7].fat },
+        { y: selectedMeals[8].fat },
+        { y: selectedMeals[9].fat }
+      ]
     },
     {
       type: 'stackedBar',
       name: 'Sodium',
       showInLegend: 'true',
-      dataPoints: [{ x: selectedMeals[0].sodium, y: selectedMeals[0].meal_name }]
+      dataPoints: [
+        { y: selectedMeals[0].sodium },
+        { y: selectedMeals[1].sodium },
+        { y: selectedMeals[2].sodium },
+        { y: selectedMeals[3].sodium },
+        { y: selectedMeals[4].sodium },
+        { y: selectedMeals[5].sodium },
+        { y: selectedMeals[6].sodium },
+        { y: selectedMeals[7].sodium },
+        { y: selectedMeals[8].sodium },
+        { y: selectedMeals[9].sodium }
+      ]
     },
     {
       type: 'stackedBar',
       name: 'Cholesterol',
       showInLegend: 'true',
-      dataPoints: [{ x: selectedMeals[0].cholesterol, y: selectedMeals[0].meal_name }]
+      dataPoints: [
+        { y: selectedMeals[0].cholersterol },
+        { y: selectedMeals[1].cholersterol },
+        { y: selectedMeals[2].cholersterol },
+        { y: selectedMeals[3].cholersterol },
+        { y: selectedMeals[4].cholersterol },
+        { y: selectedMeals[5].cholersterol },
+        { y: selectedMeals[6].cholersterol },
+        { y: selectedMeals[7].cholersterol },
+        { y: selectedMeals[8].cholersterol },
+        { y: selectedMeals[9].cholersterol }
+      ]
     }]
   });
   chart.render();
