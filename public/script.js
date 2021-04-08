169dbb05c8eecc4bea245977b972d541e033c72a
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-08 17:39:13 -0400

    add label for meal names

--- public/script.js
+++ public/script.js
@@ -31,7 +31,8 @@ async function macroChart() {
       text: 'Macro Data for UMD Dining Hall Meals'
     },
     axisX: {
-      title: 'Meal Name'
+      title: 'Meal Name',
+      xValueFormatString: 'string'
     },
     axisY: {
       title: 'Macro Info'
@@ -50,16 +51,16 @@ async function macroChart() {
       xValueFormatString: selectedMeals.meal_name,
       yValueFormatString: '### cal',
       dataPoints: [
-        { y: selectedMeals[0].calories },
-        { y: selectedMeals[1].calories },
-        { y: selectedMeals[2].calories },
-        { y: selectedMeals[3].calories },
-        { y: selectedMeals[4].calories },
-        { y: selectedMeals[5].calories },
-        { y: selectedMeals[6].calories },
-        { y: selectedMeals[7].calories },
-        { y: selectedMeals[8].calories },
-        { y: selectedMeals[9].calories }
+        { y: selectedMeals[0].calories, label: selectedMeals[0].meal_name},
+        { y: selectedMeals[1].calories, label: selectedMeals[1].meal_name },
+        { y: selectedMeals[2].calories, label: selectedMeals[2].meal_name },
+        { y: selectedMeals[3].calories, label: selectedMeals[3].meal_name },
+        { y: selectedMeals[4].calories, label: selectedMeals[4].meal_name },
+        { y: selectedMeals[5].calories, label: selectedMeals[5].meal_name },
+        { y: selectedMeals[6].calories, label: selectedMeals[6].meal_name },
+        { y: selectedMeals[7].calories, label: selectedMeals[7].meal_name },
+        { y: selectedMeals[8].calories, label: selectedMeals[8].meal_name },
+        { y: selectedMeals[9].calories, label: selectedMeals[9].meal_name }
       ]
     },
     {
